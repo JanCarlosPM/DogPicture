198d2281a78b7fff15ccaf34873a6b8a56b434ed
janca <jan.prado133873@est.uca.edu.ni>
2023-03-09 00:44:05 -0600

    Otra manera de conseguir la foto del perro xd

--- app/src/main/java/com/example/dogpicture/MainActivity.java
+++ app/src/main/java/com/example/dogpicture/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.dogpicture;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.android.volley.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView cargarImagen;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        cargarImagen = findViewById(R.id.imageView);
-        Picasso.get()
-                .load("https://images.dog.ceo/breeds/doberman/n02107142_7205.jpg")
-                .into(cargarImagen);
+        imageView = findViewById(R.id.imageView);
+        String url = "https://dog.ceo/api/breeds/image/random";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String imageUrl = response.getString("message");
+                            // descargar la imagen desde la URL obtenida
+                            Glide.with(MainActivity.this)
+                                    .load(imageUrl)
+                                    .into(imageView);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
